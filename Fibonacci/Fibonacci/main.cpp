eb42edcda212ce00c0d19be02eea1019518619cb
Hades000 <lsw463@naver.com>
2022-06-24 05:17:56 +0900

    Feat: 피보나치 값 찾는 함수 추가(재귀함수)

--- Fibonacci/Fibonacci/main.cpp
+++ Fibonacci/Fibonacci/main.cpp
@@ -24,14 +24,23 @@ int FibonacciLoop(int num)
 	return result;
 }
 
+int RecFibonacci(int num)
+{
+	if(num == 1 || num ==2)
+		return 1;
+	
+	return RecFibonacci(num-2) + RecFibonacci(num-1);
+}
+
 int main()
 {
 	int a = 0;
+	int b = 0;
 	a = FibonacciLoop(10);
+	b = RecFibonacci(10);
 
 	cout << a << endl;
-
-
+	cout << b << endl;
 
 	return 0;
 }
